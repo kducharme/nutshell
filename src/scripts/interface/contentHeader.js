d9d47c83cd0e98189abe74c58cc4bc8b99c97e5f
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-09 14:08:57 -0500

    added header

---
+++ src/scripts/interface/contentHeader.js
@@ -0,0 +1,11 @@
+const $ = require('jquery');
+
+const contentHeader = () => {
+    const fragment = document.createDocumentFragment();
+    const structure = document.createElement('span');
+    structure.classList = 'header'
+
+}
+
+module.exports = contentHeader;
+
---
+++ src/scripts/main.js
@@ -0,0 +1,3 @@
+const contentHeader = require('./interface/contentHeader');
+
+contentHeader()
