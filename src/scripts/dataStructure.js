bacdf6e994a648ddc2addd663767e13940483be9
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-09 06:59:32 -0500

    setting up database

---
+++ src/scripts/dataStructure.js
@@ -0,0 +1,74 @@
+const user = {
+    name: 'Kyle Ducharme',
+    email: 'ducharme.kyle@gmail.com',
+    events: [
+        {
+            name: 'dance recital',
+            date: 'March 1, 2018',
+            location: 'nashville software school'
+        },
+        {
+            name: 'dinner party',
+            date: 'May 22, 2018',
+            location: 'restaurant'
+        }
+    ],
+    articles: [
+        {
+            title: 'patriots win world series',
+            summary: 'really great article',
+            url: 'google.com/patriots'
+        },
+        {
+            title: 'friends is a cool show',
+            summary: 'talked about how the show is cool',
+            url: 'google.com/friends'
+        }
+    ],
+    tasks: [
+        {
+            name: 'go to the store',
+            due: 'April 29, 2018',
+            complete: true
+        },
+        {
+            name: 'learn how to code',
+            due: 'Dec 25, 2018',
+            complete: false
+        }
+    ],
+    chats: [
+        {
+            name: 'Joe Shmoe',
+            messages: [
+                {
+                    user: 'Joe Shmoe',
+                    text: 'Hello friend',
+                    date: 'April 21, 2018'
+                },
+                {
+                    user: 'Kyle Ducharme',
+                    text: 'Hey there',
+                    date: 'April 22, 2018'
+                }
+            ]
+        },
+        {
+            name: 'Jen Shmen',
+            messages: [
+                {
+                    user: 'Jen Shmen',
+                    text: 'Waddup bro',
+                    date: 'April 29, 2018'
+                },
+                {
+                    user: 'Kyle Ducharme',
+                    text: 'Hey weirdo',
+                    date: 'April 30, 2018'
+                }
+            ]
+        }
+
+    ]
+
+}
