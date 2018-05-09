a7a3e04005022e879006d7e0dce93e9fff6226c1
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-09 15:47:17 -0500

    content header actor working

--- src/scripts/interface/headerManager.js
+++ src/scripts/interface/headerManager.js
@@ -1,6 +1,6 @@
 const $ = require('jquery');
-const buttonFactory = require('../factories/buttonFactory');
 const $printArea = $('#data');
+const buttonFactory = require('../factories/buttonFactory');
 
 const headerManager = Object.create(null, {
     createStructure: {
@@ -9,7 +9,9 @@ const headerManager = Object.create(null, {
             const structure = document.createElement('span');
             structure.classList = 'header';
             const button = headerManager.addButton();
+            const filters = headerManager.addFilters();
 
+            structure.appendChild(filters)
             structure.appendChild(button)
             fragment.appendChild(structure)
             $printArea.append(fragment);
@@ -17,7 +19,33 @@ const headerManager = Object.create(null, {
     },
     addFilters: {
         value: function () {
+            const structure = document.createElement('span');
+            const filters = ['Friends', 'Events', 'Articles']
+            structure.classList = 'header__filterList';
+            filters.forEach(filter => {
+                const block = document.createElement('span');
+                block.classList = 'header__filterList--block'
 
+                const filterName = document.createElement('p');
+                filterName.classList = 'header__filterList--label'
+                filterName.textContent = filter;
+
+                const counter = document.createElement('p');
+                counter.classList = 'header__filterList--counter'
+                counter.textContent = headerManager.countFilters();
+                
+                block.appendChild(filterName)
+                block.appendChild(counter)
+                structure.appendChild(block)
+            })
+            return structure;
+        }
+    },
+    countFilters: {
+        // TODO -- HOOK UP FILTER COUNTERS
+        value: function () {
+            const count = Math.floor(Math.random() * 10);
+            return count;
         }
     },
     addButton: {
@@ -29,7 +57,8 @@ const headerManager = Object.create(null, {
     },
     createDropdown: {
         value: function () {
-
+            const structure = document.createElement('span')
+            console.log('dropdown active')
         }
     }
 })
