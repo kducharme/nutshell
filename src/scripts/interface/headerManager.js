a23c3debcfabba284e56b9f0adf0976de4cdc40f
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-09 17:09:49 -0500

    add new dropdown working

--- src/scripts/interface/headerManager.js
+++ src/scripts/interface/headerManager.js
@@ -9,39 +9,59 @@ const headerManager = Object.create(null, {
             const structure = document.createElement('span');
             structure.classList = 'header';
             const button = headerManager.addButton();
-            const filters = headerManager.addFilters();
+            const tabs = headerManager.addTabs();
 
-            structure.appendChild(filters)
+            structure.appendChild(tabs)
             structure.appendChild(button)
             fragment.appendChild(structure)
             $printArea.append(fragment);
         }
     },
-    addFilters: {
+    addTabs: {
         value: function () {
             const structure = document.createElement('span');
-            const filters = ['Friends', 'Events', 'Articles']
-            structure.classList = 'header__filterList';
-            filters.forEach(filter => {
+            const tabs = ['Friends', 'Events', 'Articles']
+            structure.classList = 'header__tabList';
+            tabs.forEach(tab => {
                 const block = document.createElement('span');
-                block.classList = 'header__filterList--block'
-
-                const filterName = document.createElement('p');
-                filterName.classList = 'header__filterList--label'
-                filterName.textContent = filter;
+                block.classList = 'header__tabList--block';
+                block.setAttribute('id', tab)
+                block.addEventListener('click', function(e) {
+                    headerManager.activeTab(e)
+                })
+                const tabName = document.createElement('p');
+                tabName.classList = 'header__tabList--label'
+                tabName.textContent = tab;
 
                 const counter = document.createElement('p');
-                counter.classList = 'header__filterList--counter'
-                counter.textContent = headerManager.countFilters();
+                counter.classList = 'header__tabList--counter'
+                counter.textContent = headerManager.countTabs();
                 
-                block.appendChild(filterName)
+                block.appendChild(tabName)
                 block.appendChild(counter)
                 structure.appendChild(block)
             })
             return structure;
         }
     },
-    countFilters: {
+    activeTab: {
+        value: function (e) {
+            let $activeTab = $('#Friends');
+            let $activeLabel = $('#Friends')[0].childNodes[0];
+            let $activeCounter = $('#Friends')[0].childNodes[1];
+            if (e) {
+                $activeTab = e.currentTarget;
+                $activeLabel = $activeTab.childNodes[0];
+                $activeCounter = $activeTab.childNodes[1];
+            }
+            $('.header__tabList--label').removeClass('activeTab');
+            $('.header__tabList--counter').removeClass('activeCounter');
+            $activeLabel.classList.add('activeTab');
+            $activeCounter.classList.add('activeCounter');
+            console.log($activeTab)
+        }
+    },
+    countTabs: {
         // TODO -- HOOK UP FILTER COUNTERS
         value: function () {
             const count = Math.floor(Math.random() * 10);
@@ -57,8 +77,25 @@ const headerManager = Object.create(null, {
     },
     createDropdown: {
         value: function () {
-            const structure = document.createElement('span')
-            console.log('dropdown active')
+            const body = document.querySelector('body')
+            const options = ['Friend', 'Event', 'Article', 'Task']
+            const background = document.createElement('span');
+            background.classList = 'dropdown__background';
+            background.addEventListener('click', function() {
+                headerManager.closeDropdown();
+            })
+            const dropdown = document.createElement('span');
+            dropdown.classList = 'dropdown';
+            dropdown.textContent = 'hey hey hey'
+
+            body.appendChild(background);
+            $('.header__button').append(dropdown)
+        }
+    },
+    closeDropdown: {
+        value: function () {
+            $('.dropdown__background').remove();
+            $('.dropdown').remove();
         }
     }
 })
