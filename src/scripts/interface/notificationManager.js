650648f0a47267fd14a0929b0f7b38762a2810b2
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-17 13:36:14 -0500

    active chat recognition working

--- src/scripts/interface/notificationManager.js
+++ src/scripts/interface/notificationManager.js
@@ -13,10 +13,27 @@ const notificationManager = Object.create(null, {
                 const newMessage = Object.keys(allChats)
                     .map(i => allChats[i])
                     .pop();
-
                 console.log(newMessage)
                 notificationManager.postNewMessage(newMessage)
+                notificationManager.showNotification(newMessage)
             })
+        }
+    },
+    showNotification: {
+        value: function (message) {
+            const activeChat = $('.activeChat')[0].id;
+            const currentUser = getCurrentUser().uid;
+            const activeUserFriendList = Array.from($('.friends__list')[0].childNodes);
+
+            // If the person that received the message is not the active channel, then it was sent to someone else
+            if ((message.receiver === activeChat && message.sender === currentUser) || (message.receiver === currentUser && message.sender === activeChat)) {
+                console.log('this chat')
+            }
+            else {
+                const recipient = message.receiver;
+                console.log(activeUserFriendList);
+                console.log('dif chat');
+            }
         }
     },
     postNewMessage: {
@@ -24,13 +41,13 @@ const notificationManager = Object.create(null, {
             const currentUser = getCurrentUser().uid;
             const activeChat = $('.activeChat')[0].id;
 
-            if ((message.sender === activeChat || message.receiver === activeChat) && (message.sender !== currentUser)) {
+            if (message.sender === activeChat && message.receiver === currentUser && message.sender !== currentUser) {
                 const $structure = $('<span>')
                     .addClass('message');
                 const $message = $('<p>')
                     .text(message.text)
                     .addClass('message__text');
-    
+
                 if (currentUser === message.sender) {
                     $structure.addClass('message__text--sender');
                     $message.addClass('message__textSender');
@@ -38,12 +55,14 @@ const notificationManager = Object.create(null, {
                 if (currentUser !== message.sender) {
                     $structure.addClass('message__text--receiver');
                 }
-    
+
                 $structure.append($message)
                 $('.friends__messages--post').append($structure);
+
                 friendManager.scrollToBottom();
             }
-
+            else {
+            }
 
         }
     }
